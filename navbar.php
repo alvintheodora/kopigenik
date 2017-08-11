30e00e0c01b7c64bbe3dafa0091359e364a91e46
alvintheodora <alvin_theodora@yahoo.com>
2017-08-11 15:46:44 +0700

    fix navbar-toggle button to 992px and li.active

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="css/kopigenik.css">
 <nav class="navbar navbar-default navbar-fixed-top">
-	<div class="container">
+	<div class="container-fluid container-navbar">
 		<div class="navbar-header">
 			<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#myNavbar">
 				<span class="icon-bar"></span>
@@ -11,7 +11,7 @@
 		</div>
 		<div class="collapse navbar-collapse" id="myNavbar">
 			<ul class="nav navbar-nav navbar-right">
-				<li class="active"><a href="subscribe.php">SUBSCRIBE</a></li>
+				<li><a href="subscribe.php">SUBSCRIBE</a></li>
 				<li><a href="about-us.php">WHAT IS KOPIGENIK</a></li>
 				<li><a href="beans.php">BEANS</a></li>
 				<li><a href="faq.php">FAQ</a></li>
@@ -20,3 +20,13 @@
 		</div>
 	</div>
 </nav>
+
+<script type="text/javascript">
+	$pathnameLocal = (location.pathname).substr(18);//18 karena potong tulisan /kopigenik-master/
+	$pathnameHosting = (location.pathname).substr(1);//1 karena potong /
+	$(".nav li").each(function(){		
+		if($(this).children().attr("href") == $pathnameHosting){
+			$(this).addClass("active");
+		}
+	});
+</script>
