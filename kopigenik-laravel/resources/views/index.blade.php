6d5799c9d65f4d92288638821ac4002cfe015649
Alvin T <alvin@test.com>
2017-09-22 10:03:45 +0700

    set layout.app

---
+++ kopigenik-laravel/resources/views/index.blade.php
@@ -0,0 +1,162 @@
+@extends('layouts.app')
+
+@section('title','Index')
+
+@section('content')
+	<!--top-->
+	<div class="container-outer">
+		<div class="container-fluid banner-main">						
+			<div class="row layer-tagline">
+				<div class="slide-logo">				
+					<img src="asset/kopigenik-icon.png" style="transform: translateX(80%); padding-top: 30px;" width="90" height="110">
+					<p style="transform: translate(45%,380%); font-size: 24px; color: #fff;">KOPIGENIK</p>
+					<p style="transform: translate(67%,600%); font-size: 14px; color: #fff;">SINCE 2017</p>
+				</div>	
+				<div class="tagline">
+					<h2 style="margin-left: -27%; color: #fff; font-weight: 500; font-size: 36px;">- Indonesia's Best Coffee Subscription -</h2>
+					<h3 style="margin-left: -10%; color: #fff; left: 50%; font-size: 28px;">Delivering coffee to your door</h3>
+				</div>
+				
+				<div class="col-xs-4 pl-3 layer layer-1">				
+					<div class="banner-info">
+						<h3 class="black" style="color: #fff;">Watch videos about your coffee</h3>
+						<p class="black" style="color: #fff;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius</p>
+						<a href="videos.php" class="btn btn-banner" style="margin-bottom: 20px; color: #fff;">See videos <span class="glyphicon glyphicon-menu-right" style="margin-left: 5px;"></span></a>
+					</div>										
+					<img class="img-responsive" src="http://mobilemarketingmagazine.com/wp-content/uploads/2016/10/Branded-Moments-Transparent-background_b.png" alt="coffee videos" style="margin-left: -9%;">
+				</div>		
+
+				<div class="col-xs-4 pl-3 layer layer-2">				
+					<div class="banner-info">
+						<h3 class="black" style="color: #fff;">Enjoy your coffee with subscription</h3>
+						<p class="black" style="color: #fff;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius</p>
+						<a href="videos.php" class="btn btn-banner" style="margin-bottom: 20px; color: #fff;">See videos <span class="glyphicon glyphicon-menu-right" style="margin-left: 5px;"></span></a>
+					</div>	
+					<img class="img-responsive" src="asset/coffee_bag3.png" alt="subscribe board">				
+				</div>
+
+				<div class="col-xs-4 pl-3 layer layer-3">				
+					<div class="banner-info">
+						<h3 class="black" style="color: #fff;">We deliver first-grade beans</h3>
+						<p class="black" style="color: #fff;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius</p>
+						<a href="videos.php" class="btn btn-banner" style="margin-bottom: 20px; color: #fff;">See videos <span class="glyphicon glyphicon-menu-right" style="margin-left: 5px;"></span></a>
+					</div>
+					<img class="img-responsive" src="asset/coffee_beans2.png" align="coffee beans">		
+				</div>
+			</div>				
+		</div>
+	</div>
+	
+
+	<!--top2-->
+	<div class="container-fluid banner-main-md" style="color: white;">		
+		<!--<h1>K O P I G E N I K</h1>-->
+		<div class="row">
+			<div class="col-xs-6">
+				<h3 class="black font-header-mobile">Watch videos about your coffee</h3>
+				<p class="black font-p-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius</p>
+				
+			</div>
+			<div class="col-xs-6">
+				<img class="img-responsive" src="asset/video-banner.png" alt="coffee videos" style="margin-top: 20px;">
+			</div>
+
+		</div>
+		<a href="#" class="btn btn-block btn-banner font-header-mobile" style="margin-top: 10px; margin-bottom: 20px;">See videos <span class="glyphicon glyphicon-menu-right" style="margin-left: 5px;"></span></a>
+		<hr>														
+		<div class="row">
+			<div class="col-xs-6">
+				<img class="img-responsive" src="asset/coffee_bag3.png" alt="subscribe board" style="margin-top: 20px;">
+			</div>
+			<div class="col-xs-6">
+				<h3 class="black font-header-mobile">Enjoy your coffee with subscription</h3>
+				<p class="black font-p-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius</p>
+				
+			</div>
+		</div>
+		<a href="subscribe.php" class="btn btn-block btn-banner font-header-mobile" style="margin-bottom: 20px;">See subscription plans <span class="glyphicon glyphicon-menu-right" style="margin-left: 5px;"></span></a>	
+		<hr>											
+		<div class="row">
+			<div class="col-xs-6">
+				<h3 class="black font-header-mobile">We deliver first-grade beans</h3>
+				<p class="black font-p-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius</p>
+				
+			</div>
+			<div class="col-xs-6">
+				<img class="img-responsive" src="asset/coffee_beans2.png" alt="coffee beans" style="margin-top: 20px;">
+			</div>
+		</div>	
+		<a href="beans.php" class="btn btn-block btn-banner font-header-mobile" style="margin-bottom: 20px;">See our beans <span class="glyphicon glyphicon-menu-right" style="margin-left: 5px;"></span></a>	
+		<hr>					
+	</div>
+
+
+	<!--how to kopigenik-->
+	<div class="container-fluid container-inner">
+		<div class="section-block">
+			<h2 class="text-center font-header">How To Kopigenik</h2>
+			<div class="underline underline-200"></div>	
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-12">				
+				<div class="how-to-kopigenik-banner"></div>				
+			</div>
+			
+		</div>
+	</div>
+	
+	<!--<hr class="line">-->
+
+	<!--video-->
+	<div class="container-fluid container-full bg-grey">
+		<div class="row container-fixed">
+			<div class="col-sm-5 col-sm-offset-7 video-info">
+				<h2 class="font-header">Watch Kopigenik's Top-Choice Video</h2>
+				<p class="font-info">Enhance your skills about coffee from high-end baristas around the world.</p>
+				<button class="btn btn-lg btn-banner">See videos</button>			
+			</div>									
+		</div>
+		<div class="video-overflow">
+			<div class="video-banner"></div>
+			<video autoplay loop preload="auto" class="video">
+				<source src="https://media.pactcoffee.com/video/coffee_hero.mp4" type="video/mp4">
+				Your browser does not support the video tag.
+			</video>	
+		</div>
+	</div>
+
+	<!--<hr class="line">-->
+
+	<!--blog-->
+	<div class="container-fluid container-inner text-center">
+		<div class="section-block">
+			<h2 class="text-center font-header">Blog for Coffee Enthusiast</h2>		
+			<div class="underline underline-200"></div>	
+		</div>
+		<div class="row">
+			<a href="blog.php" class="link-text">
+				<div class="col-sm-6">
+					<div class="shadow">
+						<img class="img-responsive center-block slideanim" src="asset/original/rsz_blog_espresso.jpg" alt="espresso extraction">				
+						<div style="padding: 20px;">					
+							<p style="color: grey;">WEEKLY BLOG</p>
+							<p style="font-size: 24px; color: #000;">Learn how to make your coffee</p>
+						</div>
+					</div>		
+				</div>
+			</a>
+			<a href="blog.php" class="link-text">
+				<div class="col-sm-6">	
+					<div class="shadow">
+						<img class="img-responsive center-block slideanim" src="asset/original/rsz_blog_drink_coffee.jpg" alt="time to drink coffee">				
+						<div style="padding: 20px;">					
+							<p style="color: grey;">WEEKLY BLOG</p>
+							<p style="font-size: 24px; color: #000;">Best time to drink your coffee</p>
+						</div>
+					</div>		
+				</div>
+			</a>
+		</div>
+	</div>
+@endsection
--- kopigenik-laravel/resources/views/layouts/app.blade.php
+++ kopigenik-laravel/resources/views/layouts/app.blade.php
@@ -8,73 +8,114 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Styles --> 
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/kopigenik.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
+        <!-- Navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid container-navbar">
                 <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#myNavbar">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                    <a class="navbar-brand" href="{{ url('/') }}"><img class="img-responsive" src="asset/kopigenikbanner.png"></a>
+
                 </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
+                <div class="collapse navbar-collapse navbarKu" id="myNavbar">
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
+                        <li class="navBurger"><a class="navbarKu" href="subscribe.php">SUBSCRIBE</a></li>
+                        <li class="navBurger"><a class="navbarKu" href="beans.php">BEANS</a></li>
+                        <li class="navBurger"><a class="navbarKu" href="videos.php">VIDEOS</a></li>
+                        <li class="navBurger"><a class="navbarKu" href="about-us.php">OUR STORY</a></li>
+                        <li class="navBurger"><a class="navbarKu" href="faq.php">FAQ</a></li>
+                        <li class="navBurger"><a class="navbarKu" href="blog.php">BLOG</a></li>
+                        <li class="navBurger"><a class="navbarKu" href="{{ route('login') }}">LOGIN</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
+        <!-- navbar-fixed clear -->
+        <div class="after-navbar" style="margin-bottom: 60px;"></div>
 
+        <!-- Content -->
         @yield('content')
+
+        <!--Footer-->
+        <footer class="footer-btm container-fluid container-inner bg-black1">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h5 style="color: #f7db9c;">QUICK LINKS</h5>
+                    <ul>
+                        <li><a href="subscribe.php">Subscribe</a></li>
+                        <li><a href="beans.php">Beans</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h5 style="color: #f7db9c;">ABOUT US</h5>
+                    <ul>
+                        <li><a href="about-us.php">What is Kopigenik</a></li>
+                        <li><a href="faq.php">FAQ</a></li>
+                        <li><a href="blog.php">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h5 style="color: #f7db9c;">SOCIAL MEDIA</h5>
+                    <ul>
+                        <li>
+                            <a href="https://facebook.com/kopigenik" target="__blank">
+                                <img class="icon-social-media" src="asset/icon-facebook.svg">
+                                <span>Kopigenik_id</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://instagram.com/kopigenik" target="__blank"">
+                                <img class="icon-social-media" src="asset/icon-instagram.svg">
+                                <span>Kopigenik</span>
+                            </a>
+                        </li>
+                        <li>                        
+                            <img class="icon-social-media" src="asset/icon-line.jpg">
+                            <span style="color: #fff;">@Kopigenik</span>                            
+                        </li>
+                        <li style="margin-left: 50px;">
+                            <div class="line-it-button" data-lang="en" data-type="friend" data-lineid="@kopigenik" data-count="true" data-home="true" style="display: none; padding-top: 5px;"></div>                            
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/kopigenik.js"></script>
+	<script type="text/javascript" src="js/velocity.js"></script>
+    <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+    <script type="text/javascript">
+            $pathnameLocal = (location.pathname).substr(18);//18 karena potong tulisan /kopigenik-master/
+            $pathnameHosting = (location.pathname).substr(1);//1 karena potong /
+            $(".nav li").each(function(){       
+                if($(this).children().attr("href") == $pathnameLocal){
+                    $(this).addClass("active");
+                }
+            });
+    </script>
+    
 </body>
 </html>
