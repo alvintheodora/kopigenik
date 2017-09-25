b83f319d7aad6b324dcae99b2f4108e5fc9e27e8
Alvin T <alvin@test.com>
2017-09-26 00:25:12 +0700

    add Zizaco/entrust lib

--- kopigenik-laravel/config/auth.php
+++ kopigenik-laravel/config/auth.php
@@ -1,4 +1,4 @@
-<?php
+f<?php
 
 return [
 
@@ -68,6 +68,7 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
+            'table' => 'users',
         ],
 
         // 'users' => [
