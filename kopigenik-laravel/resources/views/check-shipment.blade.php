1272a132d3ac04b1da198232238bd1e2ce67b429
Alvin Theodora <alvintheodoraa@gmail.com>
2017-10-13 17:44:23 +0700

    improve my subscription view

--- kopigenik-laravel/resources/views/check-shipment.blade.php
+++ kopigenik-laravel/resources/views/check-shipment.blade.php
@@ -7,24 +7,74 @@
 	<div class="container-fluid">
 		<h1 class="text-center">My Subscriptions</h1>
 		<h2>On Progress</h2>
-		<ul>
-			@foreach($shipments_user as $shipment)				
-				
-					<li><a href="\check-shipments\{{$shipment->id}}">ID: {{$shipment->transaction->id}}</a>, 
-						Plan: {{$shipment->transaction->plan->name}} ({{$shipment->transaction->subscribe_duration}} months), 
-						Delivery Address: {{$shipment->address . ', ' . $shipment->district . ', ' . $shipment->city . ', ' . $shipment->province . ', ' . $shipment->zipcode}}, 
-						Total shipment left: {{$shipment->total_shipment_left}}
-					</li>
-				
+
+		@if($shipments_user->count()>0)
+			<div class="row">
+				<div class="col-sm-1">
+					ID
+				</div>
+				<div class="col-sm-2">
+					Plan
+				</div>
+				<div class="col-sm-4">
+					Delivery Address
+				</div>
+				<div class="col-sm-2">
+					Total shipment left
+				</div>					
+			</div>
+		
+			@foreach($shipments_user as $shipment)					
+				<div class="row">
+					<div class="col-sm-1">
+						<a href="\check-shipments\{{$shipment->id}}">{{$shipment->transaction->id}}</a>
+					</div>
+					<div class="col-sm-2">
+						{{$shipment->transaction->plan->name}} ({{$shipment->transaction->subscribe_duration}} months)
+					</div>
+					<div class="col-sm-4">
+						{{$shipment->address . ', ' . $shipment->district . ', ' . $shipment->city . ', ' . $shipment->province . ', ' . $shipment->zipcode}}
+					</div>
+					<div class="col-sm-2">
+						{{$shipment->total_shipment_left}}
+					</div>							
+				</div>				
 			@endforeach
-		</ul>
+			
+		@endif
 
 		<h2>On Hold</h2>
-		<ul>
-			@foreach($shipments_user_tb as $shipment)				
-			
-					<li><a href="\check-shipments\{{$shipment->id}}">ID: {{$shipment->transaction->id}}</a>, Plan: {{$shipment->transaction->plan->name}} ({{$shipment->transaction->subscribe_duration}} months), Time bought: {{$shipment->transaction->time_bought}}, Status: {{$shipment->transaction->status}}
-
+		@if($shipments_user_tb->count()>0)
+			<div class="row">
+				<div class="col-sm-1">
+					ID
+				</div>
+				<div class="col-sm-2">
+					Plan
+				</div>
+				<div class="col-sm-2">
+					Time bought
+				</div>
+				<div class="col-sm-2">
+					Status
+				</div>					
+			</div>
+		
+			@foreach($shipments_user_tb as $shipment)					
+				<div class="row" style="height: 50px;">
+					<div class="col-sm-1">
+						<a href="\check-shipments\{{$shipment->id}}">{{$shipment->transaction->id}}</a>
+					</div>
+					<div class="col-sm-2">
+						{{$shipment->transaction->plan->name}} ({{$shipment->transaction->subscribe_duration}} months)
+					</div>
+					<div class="col-sm-2">
+						{{$shipment->transaction->time_bought}}
+					</div>
+					<div class="col-sm-2" style="width: 11.5%;">
+						{{$shipment->transaction->status}}
+					</div>	
+					<div class="col-sm-4">
 						@if($shipment->transaction->status=="to be confirmed")
 							<a class="btn btn-primary" style="margin-left: 10px;" href="/payment-confirmation/{{$shipment->transaction->id}}">Show payment confirmation detail</a>
 
@@ -52,21 +102,44 @@
 								</div>
 							</form>
 						@endif
-
-					</li>
+					</div>				
+				</div>
 				
 			@endforeach
-		</ul>
+
+		@endif
 
 		<h2>Finished Orders</h2>
-		<ul>
-			@foreach($shipments_user_finished as $shipment)				
-				
-					<li><a href="\check-shipments\{{$shipment->id}}">ID: {{$shipment->transaction->id}}</a>, Plan: {{$shipment->transaction->plan->name}} ({{$shipment->transaction->subscribe_duration}} months), Last Delivery Date: {{$shipment->last_delivery_date}}
-					</li>
-				
+
+		@if($shipments_user_finished->count()>0)
+			<div class="row">
+				<div class="col-sm-1">
+					ID
+				</div>
+				<div class="col-sm-2">
+					Plan
+				</div>
+				<div class="col-sm-5">
+					Last Delivery Date
+				</div>				
+			</div>
+		
+			@foreach($shipments_user_finished as $shipment)					
+				<div class="row">
+					<div class="col-sm-1">
+						<a href="\check-shipments\{{$shipment->id}}">{{$shipment->transaction->id}}</a>
+					</div>
+					<div class="col-sm-2">
+						{{$shipment->transaction->plan->name}} ({{$shipment->transaction->subscribe_duration}} months)
+					</div>
+					<div class="col-sm-5">
+						{{$shipment->last_delivery_date}}
+					</div>							
+				</div>				
 			@endforeach
-		</ul>
+			
+		@endif
+
 
 	</div>
 @endsection
