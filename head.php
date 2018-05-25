c51b50bb7a0783884be7318dc4e304c7c84b974a
Alvin <alvintheodoraa@gmail.com>
2018-05-25 11:55:12 +0700

    move kopigenik-laravel to root

--- head.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/kopigenik.css">
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/kopigenik.js"></script>
--- kopigenik-master/README.md
+++
@@ -1,2 +0,0 @@
-# kopigenik
-kopigenik website
